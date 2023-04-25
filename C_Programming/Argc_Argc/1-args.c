fa1c384b699ab572331ec77cc3d3cf2336f41702
Zeb-0 <morandezeb@gmail.com>
2023-04-25 17:50:55 +0300

    more on this

---
+++ C_Programming/Argc_Argc/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of arguments passed to it
+ * Returns: 0
+ */
+
+int main(int argc, char *argv[]){
+//	if (argc > 0){
+//		printf("%d\n", argc);
+//	}
+	printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ C_Programming/Argc_Argc/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]){
+	int i;
+	if (argc > 0){
+		for (i = 0; i < argc; i++){
+			printf("%s\n", argv[i]);
+		}
+	}
+	return (0);
+}
