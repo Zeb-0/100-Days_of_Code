54c808ab290fb27931381cd252a7207cf800e446
Zeb-0 <morandezeb@gmail.com>
2023-04-25 17:33:23 +0300

    my name

---
+++ C_Programming/Argc_Argc/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints it name
+ * return: 0
+ */
+
+int main(int argc, char *argv[]){
+	if (argc > 0){
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
