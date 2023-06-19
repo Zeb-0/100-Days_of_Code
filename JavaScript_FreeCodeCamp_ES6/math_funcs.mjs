4e9e4e3bba9156b2195a8310939d324e4cb472e4
Zeb-0 <morandezeb@gmail.com>
2023-06-19 15:07:30 +0300

    some export import stuff

---
+++ JavaScript_FreeCodeCamp_ES6/math_funcs.mjs
@@ -0,0 +1,13 @@
+// work examples.
+
+const add = (x, y) => {
+	return x + y;
+}
+const sub = (a, b) => {
+	return a - b;
+}
+const mul = (a, b) => {
+	return a * b;
+
+}
+module.exports = { add, sub, mul };
