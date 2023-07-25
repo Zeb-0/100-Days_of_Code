f94bf8c5dcdc48ec78d946749fadd863d98ac91e
Zeb-0 <morandezeb@gmail.com>
2023-07-25 13:48:23 +0300

    updates

--- work-stuff/greetings.c
+++ work-stuff/greetings.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // some practice on strimgs
-printf('Hello. Welcome to our greetings session.');
+int main(){
+	char Name;
+
+	printf("Hello. Welcome to our greetings session.\nWhat is your name?\n");
+	scanf("%s", &Name);
+	if (Name.length < 8){
+		printf("Name is too short.\n");
+	}
+	else
+		printf("Hi %s. I am your assistant\n", Name);
+	return (0);
+}
