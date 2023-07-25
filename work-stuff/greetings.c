b1f2bbb553e0e6e3d14921d3fe72a207aca3c2ba
Zeb-0 <morandezeb@gmail.com>
2023-07-25 14:34:35 +0300

    added packages - only package numbers though

--- work-stuff/greetings.c
+++ work-stuff/greetings.c
@@ -5,6 +5,9 @@
 // some practice on strimgs
 int main(){
 	char Name[100];
+	int Package;
+	char Pname;
+	int i;
 
 	printf("Hello. Welcome to our greetings session.\nWhat is your name?\n");
 	scanf("%99s", Name);
@@ -12,6 +15,11 @@ int main(){
 		printf("Name is too short.\n");
 	}
 	else
-		printf("Hi %s. I am your assistant\n", Name);
+		printf("Hi %s. I am your assistant\n\n", Name);
+	printf("Here are the greetings packages that we offer.\nSelect one.\n");
+
+//	Package = 3;
+	for (Package = 0; Package <= 3; Package++)
+		printf("%d\n", Package);
 	return (0);
 }
