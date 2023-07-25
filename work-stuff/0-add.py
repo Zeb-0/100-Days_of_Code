ad783efd39255b5502362eb3aa50e9c09c69391f
Zeb-0 <morandezeb@gmail.com>
2023-07-25 12:47:54 +0300

    some functions

---
+++ work-stuff/0-add.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    result = a + b
+    return result
+result = add(2, 3)
+print(result)
+
+print()
+
+def mul(x, y):
+    ans = x * y
+    return ans
+res = mul(2, 3)
+print(res)
+
+print()
+
+def sub(a, b):
+    minus = b - a
+    return minus
+answ = sub(2, 3)
+print(answ)
+
+print()
+
+def rem(n):
+    mod = n % 10
+    return mod
+remainder = rem(23)
+print(remainder)
