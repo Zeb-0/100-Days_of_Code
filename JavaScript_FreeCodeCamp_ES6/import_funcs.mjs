55580e3a0875bf69f0d8a5c58efa2ce092adb01c
Zeb-0 <morandezeb@gmail.com>
2023-07-26 19:33:06 +0300

    updates

--- JavaScript_FreeCodeCamp_ES6/import_funcs.mjs
+++ JavaScript_FreeCodeCamp_ES6/import_funcs.mjs
@@ -1,4 +1,4 @@
-import { add, sub, mul } from './math_funcs.mjs';
-// console.log(add(2, 3));
+import { add, sub, mul } from './math_funcs.js';
+console.log(add(2, 3));
 console.log(sub(5, 2));
 console.log(mul(2, 10));
