8161d568f9610d44f2bbe672784f291097d982c3
Zeb-0 <morandezeb@gmail.com>
2023-07-26 08:59:52 +0300

    corrections

--- work-stuff/greetings.c
+++ work-stuff/greetings.c
@@ -31,19 +31,11 @@ int main(){
 	int choice;
 
 	do {
-<<<<<<< HEAD
 		printf("\nEnter choice: ");
 		scanf("%d)\n", &choice);
 
 		if (choice > 3){
 			printf("Invalid choice. Please enter a Valid choice.\n");
-=======
-		printf("\nEnter Pno: ");
-		scanf("%d)\n", &choice);
-
-		if (choice > 3){
-			printf("Invalid choice. Please enter a Vlid choice.\n");
->>>>>>> 5bfc12ac0d9408c615253962096d2f25516986f9
 			continue;
 		}
 		else {
