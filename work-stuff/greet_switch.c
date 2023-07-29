d4222c258a7ee83e5276c6c8260ab6d0bbeba156
Zeb-0 <morandezeb@gmail.com>
2023-07-29 09:15:30 +0300

    update switch statement

--- work-stuff/greet_switch.c
+++ work-stuff/greet_switch.c
@@ -3,16 +3,20 @@
 // trying tk implement a switch case
 
 int main(){
-	int choice = 3;
+	int choice = 4;
 	char Name[100];
-	char Pname[][1000] = {"Morning", "Noon time", "Evening"};
+	char Pname[][1000] = {"Morning", "Noontime", "Evening"};
 	int Pnumber;
 
 	switch(choice) {
 		case 1: // check if choice is morning
 			if (choice == 1){
-				printf("Good morning %s ?\n", Name);
-				break;
+				printf("Good morning %s ? How are you doing today?\n", Name);
+				scanf("%99s", response);
+				printf("\nThat's great to hear.\nHave a wonderful day.\n");
 			}
+		default:
+			printf("Please enter a valid choice\n");
+			break;
 	}
 }
