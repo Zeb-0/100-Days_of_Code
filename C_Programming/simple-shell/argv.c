717d13c0e2afe74af0579f795089e27cb6246837
Zeb-0 <morandezeb@gmail.com>
2023-08-02 10:03:49 -0700

    argument vector

---
+++ C_Programming/simple-shell/argv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// prints all arguments passed to the prompt
+
+int main(int argc, char *argv[]) {
+	int i;
+
+	for (i = 0; i < argc; i++) {
+		argv = argv[i];
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
