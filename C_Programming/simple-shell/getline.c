3980709845e3c4db491e14cd5b6a4134c65d19e4
Zeb-0 <morandezeb@gmail.com>
2023-08-03 18:54:06 +0300

    getline function

---
+++ C_Programming/simple-shell/getline.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// trying out the getline function
+/**
+ * getline - continuously prompts user for input
+ * by printing `$ ` first
+ * stores the input in a buffer
+ * then prints to stdout
+ */
+int main(){
+	char *buffer;	// pointer to store our input
+	size_t len;	// size of input line
+	
+	while(True) {
+		printf("$ ");
+		getline(&buffer, &len, stdin);
+		pr8ntf("%s\n", buffer);
+
+		return (0);
+}
