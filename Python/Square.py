2138375b70a77c0324ca47905ba603c46b3be848
Zeb-0 <morandezeb@gmail.com>
2023-08-08 17:48:20 +0300

    class square

---
+++ Python/Square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# figuring out About classes
+
+class Square:
+    ''' Class Square '''
+    def __init__(self, size):
+        ''' initialize class instance with 
+        private attr `size`
+        '''
+        self._size = size
