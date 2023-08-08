37e8d01734bbaedeeda553b2c4f13db348bdf5ec
Zeb-0 <morandezeb@gmail.com>
2023-08-08 18:14:40 +0300

    class Square updated

--- Python/Square.py
+++ Python/Square.py
@@ -12,8 +12,11 @@ class Square:
             size(int) - size of square
         '''
         
-        if type(size) not int:
+        if type(size) is not int:
             raise TypeError("size must be integer")
         elif size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
