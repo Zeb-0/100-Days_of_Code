a3a519f3e787f8f6f85913ca9ae098f351122ada
Zeb-0 <morandezeb@gmail.com>
2023-08-09 21:58:49 +0300

    fixing the test_Square - Unittests

--- Python/tests/test_square.py
+++ Python/tests/test_square.py
@@ -2,8 +2,29 @@
 
 import unittest
 import math
+import sys
 
+sys.path.append('../models')
+
+from Square import Square
 
 class testSquare(unittest.TestCase):
 
+    # test area method
+    def test_area(self):
+        squae = Square(5)
+        self.assertEqual(square.area(), 25)
 
+    # return the corect size
+    def test_size_property(self):
+        square = Square(4)
+        self.assertEqual(square.size, 4)
+
+    # invalid type for square size
+    def test_size_setter(self):
+        square = Square()
+        with self.assertRaises(ValueError):
+            square.size = -3
+
+if __name__ == '__main__':
+    unittest.main()
