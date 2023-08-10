a9fb08f5f1b60d88d6186363065163cf77a9d21d
Zeb-0 <morandezeb@gmail.com>
2023-08-10 16:30:39 +0300

    tests

---
+++ Python/models/funcs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+''' module containing functions '''
+
+class Funcs:
+    
+    def add(self, a, b):
+        return a + b
+
+    def minus(self, a, b):
+        return b - a
+
+    def mul(self, a, b):
+        return a * b
+
+    def square(self, a, b):
+        return a ** b
+
+    def div(self, a, b):
+        return a / b
+
