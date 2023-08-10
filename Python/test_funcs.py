3be7a0933779299fba980c6fc55f8f9cfd68dbab
Zeb-0 <morandezeb@gmail.com>
2023-08-10 15:27:30 +0300

    still working on tests

---
+++ Python/test_funcs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+''' test file for module `funcs` '''
+import unittest
+import funcs
+
+
+class test_funcs:
+
+    def test_add(self):
+        add = add(2, 3)
+        self.assertEqual(add, 5)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
