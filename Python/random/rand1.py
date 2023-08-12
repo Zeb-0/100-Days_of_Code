93250beef1ad2775178702c8b12efb29694e5b38
Zeb-0 <morandezeb@gmail.com>
2023-08-12 13:42:25 +0300

    union function

--- Python/random/rand1.py
+++ Python/random/rand1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 # use union() function
+'''
+union() is used to join two sets and returns a new set without duplicates
+'''
 
 my_set = {1, 2, 3}
 result = my_set.union({3, 4, 5})
