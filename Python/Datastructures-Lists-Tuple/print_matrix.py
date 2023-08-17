4ef4c03e309e3f27245038085a0e239778a5f9e0
Zeb-0 <morandezeb@gmail.com>
2023-08-17 18:21:59 +0300

    matrix corrections

--- Python/Datastructures-Lists-Tuple/print_matrix.py
+++ Python/Datastructures-Lists-Tuple/print_matrix.py
@@ -3,5 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            if col != row:
-                print("{:d}".fomat(col))
+            if col != row[-1]:
+                print("{:d}".format(col))
+            else: print()
