f18927c063ef69bd4c28c84d07d3a52d50df1107
Zeb-0 <morandezeb@gmail.com>
2023-08-17 22:35:01 +0300

    return a tuple from sentence

---
+++ Python/Datastructures-Lists-Tuple/mult_returns.py
@@ -0,0 +1,9 @@
+# returns length of a string and the first character in the string
+
+def multiple_returns(sentence):
+    ln = len(sentence)
+    if len(sentence) == 0:
+        sentence[0] = None
+    for i in (len(sentence)):
+        fist_char = sentence[0]
+    return(len, first_char)
