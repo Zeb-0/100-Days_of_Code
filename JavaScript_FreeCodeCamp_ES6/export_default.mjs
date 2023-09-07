e0dadbdfe42d837cd378dccdbb831571ae5f79e2
Zeb-0 <morandezeb@gmail.com>
2023-09-07 19:46:20 +0300

    corrections

--- JavaScript_FreeCodeCamp_ES6/export_default.mjs
+++ JavaScript_FreeCodeCamp_ES6/export_default.mjs
@@ -1,11 +1,3 @@
 export default function add(a, b) {
     return a + b;
 }
-
-export default function(a, b) {
-    return a + b;
-}
-
-export default function sub(a, b) {
-    return b - a;
-}
--- JavaScript_FreeCodeCamp_ES6/import_default_export.mjs
+++ JavaScript_FreeCodeCamp_ES6/import_default_export.mjs
@@ -1,5 +1,5 @@
 import add from './export_default.mjs';
-import sub from './export_default.mjs';
+// import sub from './export_default.mjs';
 
 console.log(add(2, 4));
-console.log(sub(3, 5));
+// console.log(sub(3, 5));
