7646c8bbc1113cd75a3a499dfb91fdebd281886c
Zeb-0 <morandezeb@gmail.com>
2023-09-07 18:07:51 +0300

    corrections

--- JavaScript_FreeCodeCamp_ES6/import_funcs.js
+++ JavaScript_FreeCodeCamp_ES6/import_funcs.js
@@ -1,7 +1,8 @@
 import { add, sub, mul } from './export.js';
 const sum = add(4, 3);
-console.log(sum);
 const dif = sub(4, 3);
-console.log(dif);
 const res = mul(4, 3);
-console.log(res)
+
+console.log(sum);
+console.log(dif);
+console.log(res);
