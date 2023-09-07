c77114f0353314697eff7bb4d368a09594eb5886
Zeb-0 <morandezeb@gmail.com>
2023-09-07 17:23:09 +0300

    export functions

---
+++ JavaScript_FreeCodeCamp_ES6/export.js
@@ -0,0 +1,21 @@
+const add = (x, y) => {
+    return x + y;
+}
+
+const sub = (x, y) => {
+    return x - y;
+}
+
+const mul = (x, y) => {
+    return x * y;
+}
+
+const square = (x, y) => {
+    return x ** y;
+}
+
+const div = (x, y) => {
+    return x / y;
+}
+
+export { add, sub, square, mul, div };
