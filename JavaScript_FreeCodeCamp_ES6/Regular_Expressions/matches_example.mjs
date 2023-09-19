af743fdade661eaa1e3719c3e127a0aa39b5b0b4
Zeb-0 <morandezeb@gmail.com>
2023-09-19 18:46:53 +0300

    updates

--- JavaScript_FreeCodeCamp_ES6/Regular_Expressions/matches_example.mjs
+++ JavaScript_FreeCodeCamp_ES6/Regular_Expressions/matches_example.mjs
@@ -1,6 +1,6 @@
 // example on etract matches
 
-console.log("hello, world!".match(/Hello/));
+console.log("Hello, world!".match(/Hello/));
 let ourStr = "Regular expressions";
-let ourRegex = /epressions/;
+let ourRegex = /expressions/;
 console.log(ourStr.match(ourRegex));
