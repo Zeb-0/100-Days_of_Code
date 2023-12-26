f4c35fa611484dbcc6dddbb5ee64fc900a7f0ecc
Zeberio <morandezeb@gmail.com>
2023-12-26 12:17:40 +0300

    some changes- routing

--- Web_Frameworks/hello.py
+++ Web_Frameworks/hello.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
 app =  Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World</p>"
+
+@app.route('/')
+def index():
+    return 'index Page'
+
+@app.route("/hello")
+def hello():
+    return "Hello, World"
