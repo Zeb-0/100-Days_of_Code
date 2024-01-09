7c0c91a49df329c78efd6b15a84e5325bfb8724e
Zeberio <morandezeb@gmail.com>
2024-01-09 20:06:10 +0300

    nvcnm

--- Web_Frameworks/hello.py
+++ Web_Frameworks/hello.py
@@ -29,3 +29,8 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
